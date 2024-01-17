13e72a46b009123bc353289a0651dfd5277d5c6f
mulzahrian <mulzahrianone@gmail.com>
2024-01-17 13:27:46 +0700

    add show data

--- CustomerOrderView/CustomerOrderView/Program.cs
+++ CustomerOrderView/CustomerOrderView/Program.cs
@@ -9,9 +9,32 @@ namespace CustomerOrderView
     {
         static void Main(string[] args)
         {
-            CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=(localdb)\Local; Initial Catalog=CustomerOrderViewer; Integrated Security=True");
+            try
+            {
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=(localdb)\Local; Initial Catalog=CustomerOrderViewer; Integrated Security=True");
 
-            IList<CustomerOrderDetailModels> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+                IList<CustomerOrderDetailModels> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach(CustomerOrderDetailModels customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: Fullname: {1} {2} (Id: {3}) = purchased {4} for {5} (Id: {6})",
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId
+
+);
+                    }
+                }
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Something went wrong {0}",ex.Message);
+            }
         }
     }
 
